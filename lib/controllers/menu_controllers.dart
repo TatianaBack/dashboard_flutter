b78f3632161b9c09691c1d82e2e52230eb963c23
TatianaBack <psitati@gmail.com>
2023-03-02 17:28:38 -0300

    menu

--- lib/controllers/menu_controllers.dart
+++ lib/controllers/menu_controllers.dart
@@ -1,4 +1,6 @@
+import 'package:dashboard_flutter/constants/style.dart';
 import 'package:dashboard_flutter/routing/routes.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class MenuController extends GetxController {
@@ -17,4 +19,23 @@ class MenuController extends GetxController {
   isActive(String itemName) => activeItem.value == itemName;
 
   isHovering(String itemName) => hoverItem.value == itemName;
+
+  Widget returnIconFor(String itemName) {
+    switch(itemName){
+      case OverViewPageRoute: return _customIcon(Icons.trending_up, itemName)
+    }
+  }
+
+  Widget _customIcon(IconData icon, String itemName) {
+    if (isActive(itemName))
+      return Icon(
+        icon,
+        size: 22,
+        color: dark,
+      );
+    return Icon(
+      icon,
+      color: isHovering(itemName) ? dark : lightGrey,
+    );
+  }
 }
